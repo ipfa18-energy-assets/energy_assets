faa341ef0b567410148fd195810b18c9f27b90d2
Jenny <jfcong@berkeley.edu>
2018-11-05 22:40:41 -0800

    finished draft of smart-contract

--- energy_assets_backend/contracts/Main.sol
+++ energy_assets_backend/contracts/Main.sol
@@ -1,4 +1,12 @@
 pragma solidity ^0.4.25;
+
+/* Documentation found here https://fiatcontract.com/ */
+contract FiatContract {
+  function ETH(uint _id) constant public returns (uint256);
+  function USD(uint _id) constant public returns (uint256);
+  function updatedAt(uint _id) constant public returns (uint);
+}
+
 contract Main {
 
   /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -7,6 +15,22 @@ contract Main {
   uint creditPerUnitOfCharge = 1;
   uint dollarPerUnitOfCharge = 900; // How much users get when they finish charging
   uint weiPerCredit = 1000; // How much each credit is sold for
+  FiatContract currencyConverter = FiatContract(0x2CDe56E5c8235D6360CCbb0c57Ce248Ca9C80909);
+  // MAINNET address: 0x8055d0504666e2B6942BeB8D6014c964658Ca591
+  // TESTNET address (Ropsten): 0x2CDe56E5c8235D6360CCbb0c57Ce248Ca9C80909
+
+  function USDtoETH(uint dollar) constant private returns (uint) {
+    // returns $0.01 ETH wei
+    uint256 ethCent = currencyConverter.USD(0);
+    // $0.01 * 100 * USD = USD * $1.00
+    return ethCent * 100 * dollar;
+  }
+
+  function ETHtoUSD(uint ETHinWei) constant private returns (uint) {
+    // returns $0.01 ETH wei
+    uint256 ethCent = currencyConverter.USD(0);
+    return ETHinWei / ethCent * 100;
+  }
 
   /* Keeps track of credits of approved regulated parties
   (utility companies and fossil fuel generators) */
@@ -29,6 +53,7 @@ contract Main {
   amountForSale.
   */
   mapping(address => uint) private amountForSale;
+  uint currentStartIndex;
   address[] sellers;
 
   /* Types of Acccount:
@@ -66,6 +91,7 @@ contract Main {
   /* Constructor function */
   constructor() public {
     masterAccess[msg.sender] = true;
+    currentStartIndex = 0;
   }
 
   function() public {
@@ -140,14 +166,50 @@ contract Main {
     if (addressType[msg.sender] != 2) {
       return false;
     }
-    // YOUR CODE HERE
+    if (credits[msg.sender] >= numOfCredit) {
+      credits[msg.sender] -= numOfCredit;
+      sellers.push(msg.sender);
+      amountForSale[msg.sender] += numOfCredit;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   function buy(uint numOfCredit) public returns (bool) {
     if (addressType[msg.sender] != 3) {
       return false;
     }
-    // YOUR CODE HERE
+    uint etherRequired = numOfCredit * weiPerCredit;
+    if (etherBalances[msg.sender] >= etherRequired) {
+      uint i = currentStartIndex;
+      for (i; i < sellers.length; i++) {
+        if (amountForSale[sellers[i]] >= numOfCredit) {
+          etherBalances[msg.sender] -= etherRequired;
+          amountForSale[sellers[i]] -= numOfCredit;
+          etherBalances[sellers[i]] += etherRequired;
+          credits[msg.sender] += numOfCredit;
+          currentStartIndex = i;
+          return true;
+        } else {
+          uint creditsAvailable = amountForSale[sellers[i]];
+          uint etherRequiredNow = creditsAvailable * weiPerCredit;
+          etherBalances[msg.sender] -= etherRequiredNow;
+          credits[msg.sender] += creditsAvailable;
+          amountForSale[sellers[i]] = 0;
+          etherBalances[sellers[i]] += etherRequiredNow;
+          etherRequired -= etherRequiredNow;
+          numOfCredit -= creditsAvailable;
+          delete sellers[i];
+        }
+      }
+      if (numOfCredit > 0) {
+        revert();
+        return false;
+      }
+    } else {
+      return false;
+    }
   }
 
   /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -160,14 +222,25 @@ contract Main {
   deduct x amount of credits from address. Returns true if succeeded.*/
   function verifyAndRedeem(address target, uint x) public
     _is(authorizedToRedeem) returns (bool) {
-    // YOUR CODE HERE
+    if (addressType[target] == 3 && credits[target] >= x) {
+      credits[target] -= x;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /* checks if the target address is a fossil fuel company
-  and returns the number of credits on that account. */
-  function peekAndVerify(address target) public
+  and returns the number of credits on that account. Returns 0 if
+  is not fossil fuel company */
+  /* HOW TO DIFFERENTIATE FOSSIL FUEL COMPANY HAVING 0 CREDITS? */
+  function peekAndVerify(address target) public view
     _is(authorizedToVerify) returns (uint){
-    // YOUR CODE HERE
+    if (addressType[target] == 3) {
+      return credits[target];
+    } else {
+      return 0;
+    }
   }
 
   /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -180,7 +253,15 @@ contract Main {
     if (addressType[msg.sender] != 1) {
       return false;
     }
-    // YOUR CODE HERE
+    uint amountOfEther = USDtoETH(amountInDollar);
+    address utilCompany = utilityCompanyOfUser[msg.sender];
+    if (etherBalances[utilCompany] > amountOfEther) {
+      etherBalances[utilCompany] -= amountOfEther;
+      msg.sender.transfer(amountOfEther);
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /* Withdrawal function for utility companies and fossil fuel companies */
@@ -188,7 +269,13 @@ contract Main {
     if (addressType[msg.sender] != 2 && addressType[msg.sender] != 3) {
       return false;
     }
-    // YOUR CODE HERE
+    if (etherBalances[msg.sender] >= amountInWei) {
+      etherBalances[msg.sender] -= amountInWei;
+      msg.sender.transfer(amountInWei);
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /* Deposit function for utility companies and fossil fuel companies */
@@ -196,8 +283,10 @@ contract Main {
     if (addressType[msg.sender] != 2 && addressType[msg.sender] != 3) {
       revert();
       return false;
+    } else {
+      etherBalances[msg.sender] += msg.value;
+      return true;
     }
-    // YOUR CODE HERE
   }
 
   /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -208,24 +297,25 @@ contract Main {
   keep a list of account addresses on a server. The type of each address
   can be acquired from mapping addressType[] */
 
-  function getUserBalance(address user) public
+  /* Get dollar amount of vehicle owners */
+  function getUserBalance(address user) public view
     _is(authorizedToViewUserData) returns (uint) {
-    // YOUR CODE HERE
+    return balances[user];
   }
 
-  function getUserUtilityCompany(address user) public
+  function getUserUtilityCompany(address user) public view
     _is(authorizedToViewUserData) returns (address) {
-    // YOUR CODE HERE
+    return utilityCompanyOfUser[user];
   }
 
-  function getCreditBalance(address addr) public
+  function getCreditBalance(address addr) public view
     _is(masterAccess) returns (uint) {
-    // YOUR CODE HERE
+    return credits[addr];
   }
 
-  function getEtherBalance(address addr) public
+  function getEtherBalance(address addr) public view
     _is(masterAccess) returns (uint) {
-    // YOUR CODE HERE
+    return etherBalances[addr];
   }
 
   /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -238,8 +328,14 @@ contract Main {
     bool toVerify, bool toRedeem,
     bool toChangeRates, bool toViewUserData,
     bool toUpdateAccess, bool oracle) public
-    _is(masterAccess) returns (bool) {
-    // YOUR CODE HERE
+    _is(masterAccess) {
+    masterAccess[addr] = master;
+    authorizedToVerify[addr] = toVerify;
+    authorizedToRedeem[addr] = toRedeem;
+    authorizedToChangeRates[addr] = toChangeRates;
+    authorizedToViewUserData[addr] = toViewUserData;
+    authorizedToUpdateAccess[addr] = toUpdateAccess;
+    authorizedOracle[addr] = oracle;
   }
 
   /* Quick mass granting of access for many accounts.
@@ -251,21 +347,79 @@ contract Main {
   5 authorizedToViewUserData;
   6 authorizedToUpdateAccess;
   7 authorizedOracle; */
-  function addTo(uint accessList, address[] addressList) public
+  function addTo(uint accessListID, address[] addressList) public
     _is(masterAccess) returns (bool) {
-    if (accessList < 1 || accessList > 7) {
+    if (accessListID < 1 || accessListID > 7) {
       return false;
     }
-    // YOUR CODE HERE
+    if (accessListID == 1) {
+      for (uint a = 0; a < addressList.length; a++) {
+        masterAccess[addressList[a]] = true;
+      }
+    } else if (accessListID == 2) {
+      for (uint b = 0; b < addressList.length; b++) {
+        authorizedToVerify[addressList[b]] = true;
+      }
+    } else if (accessListID == 3) {
+      for (uint c = 0; c < addressList.length; c++) {
+        authorizedToRedeem[addressList[c]] = true;
+      }
+    } else if (accessListID == 4) {
+      for (uint d = 0; d < addressList.length; d++) {
+        authorizedToChangeRates[addressList[d]] = true;
+      }
+    } else if (accessListID == 5) {
+      for (uint e = 0; e < addressList.length; e++) {
+        authorizedToViewUserData[addressList[e]] = true;
+      }
+    } else if (accessListID == 6) {
+      for (uint f = 0; f < addressList.length; f++) {
+        authorizedToUpdateAccess[addressList[f]] = true;
+      }
+    } else if (accessListID == 7) {
+      for (uint g = 0; g < addressList.length; g++) {
+        authorizedOracle[addressList[g]] = true;
+      }
+    }
+    return true;
   }
 
   /* Quick mass revoking of access for many accounts */
-  function removeFrom(uint accessList, address[] addressList) public
+  function removeFrom(uint accessListID, address[] addressList) public
     _is(masterAccess) returns (bool) {
-    if (accessList < 1 || accessList > 7) {
+    if (accessListID < 1 || accessListID > 7) {
       return false;
     }
-    // YOUR CODE HERE
+    if (accessListID == 1) {
+      for (uint aa = 0; aa < addressList.length; aa++) {
+        masterAccess[addressList[aa]] = false;
+      }
+    } else if (accessListID == 2) {
+      for (uint bb = 0; bb < addressList.length; bb++) {
+        authorizedToVerify[addressList[bb]] = false;
+      }
+    } else if (accessListID == 3) {
+      for (uint cc = 0; cc < addressList.length; cc++) {
+        authorizedToRedeem[addressList[cc]] = false;
+      }
+    } else if (accessListID == 4) {
+      for (uint dd = 0; dd < addressList.length; dd++) {
+        authorizedToChangeRates[addressList[dd]] = false;
+      }
+    } else if (accessListID == 5) {
+      for (uint ee = 0; ee < addressList.length; ee++) {
+        authorizedToViewUserData[addressList[ee]] = false;
+      }
+    } else if (accessListID == 6) {
+      for (uint ff = 0; ff < addressList.length; ff++) {
+        authorizedToUpdateAccess[addressList[ff]] = false;
+      }
+    } else if (accessListID == 7) {
+      for (uint gg = 0; gg < addressList.length; gg++) {
+        authorizedOracle[addressList[gg]] = false;
+      }
+    }
+    return true;
   }
 
 }
