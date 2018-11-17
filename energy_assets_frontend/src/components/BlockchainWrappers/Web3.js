11ee0c7d5257d0e5e508f4054616ce16e17c92ac
Michael Eliot <michael.eliot@berkeley.edu>
2018-11-17 14:57:22 -0800

    fixed a bug with web3 not loading if no local ganache instance

--- energy_assets_frontend/src/components/BlockchainWrappers/Web3.js
+++ energy_assets_frontend/src/components/BlockchainWrappers/Web3.js
@@ -1,6 +1,9 @@
 //Currently not in use because I could not get it to work
 import Web3 from 'web3';
 
-//const web3 = new Web3(window.web3.currentProvider);
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+//
+let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+if (!web3.isConnected()) {
+  web3 = new Web3(window.web3.currentProvider);
+}
 export default web3;
