49ba6a0ed9ab22e2cc5fd4e367f008b21c5e911e
Jenny <jfcong@berkeley.edu>
2018-12-02 18:40:13 -0800

    splash page update

--- energy_assets_frontend/src/App.js
+++ energy_assets_frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Button, Alert } from 'reactstrap';
+import { Button, Alert, Container, Row, Col } from 'reactstrap';
 import web3 from './web3.js';
 import mainContract from './mainContract.js';
 import './App.css';
 import VehicleOwners from './VehicleOwners.js';
 import UtilityCompanies from './UtilityCompanies.js';
 import FossilFuelCompanies from './FossilFuelCompanies.js';
+import certificate from './images/certificate.png'
+import faq from './images/faq.png'
+import blockchain from './images/blockchain.png'
 
 export default class App extends React.Component {
 
@@ -42,14 +45,47 @@ export default class App extends React.Component {
                 <br className="display-3"></br>
                 <br className="display-3"></br>
                 <h1 className="display-3">Charge. Save. Relax.</h1>
-                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                <p className="lead">The Problem: Electric vehicle users in California are not appropriately compensated for their carbon offsets</p>
                 <hr className="my-2" />
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <br className="display-3"></br>
-                <br className="display-3"></br>
-                <br className="display-3"></br>
-                <br className="display-3"></br>
-                <br className="display-3"></br>
+                <p className="lead">Our Solution: A transparent blockchain platform where credits are issued and can be sold in real time</p>
+                <br className="display-1"></br>
+                <br className="display-1"></br>
+                <br className="display-1"></br>
+                <Container>
+                  <Row>
+                    <Col>
+                      <img class="center" src={certificate} alt="LCFS credits" height="150"></img>
+                      <p className="lead">What are LCFS credits?</p>
+                      <hr className="my-2" />
+                      <p>Low Carbon Fuel Standard (LCFS) credits were created as part of a program to reduce greenhouse gases enacted through AB 32, the 2006 Global Warming Solutions Act, signed in to law by then Governor Schwarzenegger.</p>
+                    </Col>
+                    <Col>
+                      <img class="center" src={faq} alt="problem" height="150"></img>
+                      <p className="lead">What is the problem?</p>
+                      <hr className="my-2" />
+                      <p>Utility companies do not have access to the required data to track at-home charging patterns, resulting in electric vehicle owners in California not compensated appropriately for their carbon offsets.</p>
+                    </Col>
+                    <Col>
+                      <img class="center" src={blockchain} alt="blockchain" height="150"></img>
+                      <p className="lead">Why Blockchain?</p>
+                      <hr className="my-2" />
+                      <p>Blockchain provides trust among different parties by keeping accountablility in an immutable ledger, provides a distributed database, and allows energy charges to be tracked instantly.</p>
+                    </Col>
+                  </Row>
+                </Container>
+                <br className="display-1"></br>
+                <br className="display-1"></br>
+                <br className="display-1"></br>
+                <br className="display-1"></br>
+                <div className="App">
+                  <p className="lead">CONTACT US</p>
+                </div>
+                <hr className="my-2" />
+                <p className="App">Learn more, schedule a demo, or speak with a member of our team.</p>
+                <br className="display-5"></br>
+                <Button className="center" color="white">Contact Us</Button>
+                <br className="display-1"></br>
+                <br className="display-1"></br>
               </div>,
     Warning: '',
     DashBoard: ''
